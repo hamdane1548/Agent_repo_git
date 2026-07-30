9c42a083763dc5a1d049801411e088851d0e06a6
OUSSAMA HAMDANE <159944691+hamdane1548@users.noreply.github.com>
2026-07-30 18:27:12 +0100

    The First Pipline run avec sucess de get le  profile de user from the github

--- etl/crawler_link.py
+++ etl/crawler_link.py
@@ -1,3 +1,5 @@
+import loguru
+
 from data_access import GitHubProfile
 from infrastructure.base.Mongo import connection
 from Settings import Settings
@@ -5,19 +7,11 @@ settings = Settings()
 from zenml import get_step_context, step
 
 @step
-class ProfileGithub():
+def ProfileGithub(user):
     db = connection[settings.MONGO_DATABASE]
     collection = db[settings.MONGO_COLLECTION]
-    @classmethod
-    def create_user(cls, user):
-        githubuser = GitHubProfile(
-            username=user.name,
-            bio=user.bio,
-            location=user.location,
-            followers=user.followers,
-            following=user.following,
-        )
-        cls.collection.insert_one(githubuser)
-crud_db = ProfileGithub()
-
+    print(collection.find({"user_name": user}))
+    loguru.logger.success(f"create user {user.username}")
+    data = user.model_dump(mode="json")
+    collection.insert_one(data)
 
