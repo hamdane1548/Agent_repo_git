2b5e92f3eb4627b0f837763c212da4f599efabe9
OUSSAMA HAMDANE <159944691+hamdane1548@users.noreply.github.com>
2026-08-02 19:24:28 +0100

    chore: update project configuration and dependencies

--- Settings.py
+++ Settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     DATABASE_MONGO_HOST : str
     MONGO_DATABASE : str
     MONGO_COLLECTION : str
+    MONGO_COLLECTION_JOB_DESCRIPTION : str
     ## github api key
     GITHUB_API_KEY : str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
