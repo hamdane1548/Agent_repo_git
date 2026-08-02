b59c5ee5405713c7a1dd20ab53f9d25c30618d29
OUSSAMA HAMDANE <159944691+hamdane1548@users.noreply.github.com>
2026-08-02 19:25:28 +0100

    feat: integrate job description extraction into pipeline

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ def main():
     #settings.export()
     """Test to load the conf from setting"""
     github_profile_pipeline(
-        user_name="hamdane1548"
+        profile = ["hamdane1548","Ayoub-glitsh","AnouarMohamed"],
+       jobDescriptoin ="""
+       We are looking for a Backend Engineer to develop and maintain scalable web applications. The ideal candidate has experience building RESTful APIs with Spring Boot and Spring Security, designing relational databases with PostgreSQL or MySQL, and developing modern user interfaces using React.
+
+You will be responsible for implementing authentication and authorization, integrating third-party services, and ensuring high application performance and security. Experience with Docker, Git, Redis, and CI/CD pipelines is highly valued.
+
+As part of our AI initiatives, you will also collaborate on projects involving Python, LangChain, Large Language Models (LLMs), RAG (Retrieval-Augmented Generation), vector databases, and AI agents to build intelligent features into our products.
+
+Required Skills: Spring Boot, Spring Security, Java, React, TypeScript, REST APIs, PostgreSQL, Git, Docker.
+       """,
+        tech = ["spring boot","spring security","java","React","TypeScript"],
     )
 if __name__ == "__main__":
     main()
--- pipline/pipline_extractInfoGit.py
+++ pipline/pipline_extractInfoGit.py
@@ -1,11 +1,43 @@
 from zenml import pipeline
+from typing import List
+from etl import AiAgent_checkTech, createJobDescription
+from etl.SaveDataBase import SaveTheDataBase
 from etl.create_user import createUser
 from loguru import logger
 from etl.crawler_link import ProfileGithub
 @pipeline
-def github_profile_pipeline(user_name: str):
-    logger.success(f"create user {user_name}")
-    user = createUser(user_name=user_name)
-    logger.success(f"create user {user}")
-    ProfileGithub(user=user)
+def github_profile_pipeline(
+        profile : list[str],
+        jobDescriptoin : str ,
+        tech : list[str] ,
+
+        ):
+    logger.info(f"profile : {profile} , jobDescriptoin : {jobDescriptoin} , tech : {tech}")
+    if(jobDescriptoin == "" or tech == [] or profile == []):
+        logger.warning("The jobDescription or the tech or the profile is cann't be empty")
+        raise
+    """First_step create Get the Profile information from the Github"""
+    #Step  1
+    profiles_fin: list[ProfileGithub] = []
+    for profiles in profile:
+        user = createUser(profiles)
+        profiles_fin.append(user)
+    if (len(profiles) <= 0):
+        logger.warning("we can't fint any profiles form github")
+        raise
+    #Step 2
+    """Check the tech write is mismatch the job description with AI Agent """
+    TechStack = AiAgent_checkTech(jobDescriptoin,tech=tech)
+
+    #Step 3
+    """Creat the job descscption and the the profiles"""
+    job = createJobDescription(tech = TechStack,job_description=jobDescriptoin,profile = profiles_fin)
+
+    #Step 4
+    """Save the Data inot the data Base"""
+    SaveTheDataBase(job)
+
+
+
+
 
